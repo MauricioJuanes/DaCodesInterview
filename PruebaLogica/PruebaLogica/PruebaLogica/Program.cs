9e82109d5a0b03006956a08141425103f6f17fcb
Mauricio Juanes <juanesmauricio@gmail.com>
2018-08-07 06:22:49 -0500

    Agrega el metodo que calcula la dirección
    El metodo se basa en el patron que se puede ver al generar una
    matriz de resultados.
    Los resultados en la diagonal de la matriz se alternan entre R y L,
    estos valores se repiten para los elementso a la derecha de la diagonal.
    
    Abajo de la diagonal los valores se intercalan entre U y D, esos
    valores tambien se repiten hacia abajo de la diangonal
    
    Ejemplo
    
    R R R R R
    D L L L L
    D U R R R
    D U D L L
    D U D U R

--- PruebaLogica/PruebaLogica/PruebaLogica/Program.cs
+++ PruebaLogica/PruebaLogica/PruebaLogica/Program.cs
@@ -10,6 +10,62 @@ namespace PruebaLogica
     {
         static void Main(string[] args)
         {
+            var length=int.Parse( Console.ReadLine());
+            var testCases = new int[length, 2];
+            for(var i = 0; i < length; i++)
+            {
+                var line = Console.ReadLine();
+                var result = line.Split(' ').Select(x => int.Parse(x)).ToArray();
+                testCases[i, 0] = result[0];
+                testCases[i, 1] = result[1];
+            }
+
+            var results = new List<char>();
+            for (var i = 0; i < length; i++)
+            {
+                results.Add(CalculateDirection(testCases[i, 0], testCases[i, 1]));
+            }
+            foreach(var result in results)
+            {
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Doing a quick analisys I found that 
+        /// if N <= M and N is odd then the result is R
+        /// if N <= M and N is even then the result is L
+        /// if N > M and M is odd then the result is D
+        /// if N > M and M is even then the result is U
+        /// </summary>
+        /// <param name="N">rows</param>
+        /// <param name="M">columns</param>
+        /// <returns></returns>
+        static char CalculateDirection(int N, int M)
+        {
+            if (N <= M)
+            {
+                if (N % 2 != 0)
+                {
+                    return 'R';
+                }
+                else
+                {
+                    return 'L';
+                }
+            }
+            else
+            {
+                if (M % 2 != 0)
+                {
+                    return 'D';
+                }
+                else
+                {
+                    return 'U';
+                }
+            }
         }
     }
 }
